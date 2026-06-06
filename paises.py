f217b243a76a050c371a0ed9c29f83d404874c0b
Santiago <santiagonaguero@gmail.com>
2026-06-06 15:11:13 -0300

    added basic structure on fucntions

--- paises.py
+++ paises.py
@@ -1,5 +1,4 @@
 def menu():
-    paises = []
     while True:
         print("-------------------------------")
         print("Menu Principal:")
@@ -21,10 +20,9 @@ def menu():
             continue
 
         if opcion == 1:
-            agregar_paises(paises)
-
+            agregar_paises()
         if opcion == 2:
-            actualizar_paises(paises)
+            actualizar_paises()
         if opcion == 3:
             buscar_pais()
         if opcion == 4:
@@ -38,7 +36,23 @@ def menu():
             break
 
 
-def agregar_paises(paises):
+def cargar_paises():
+    paises = []
+    with open("paises.txt", "r") as archivo:
+        for linea in archivo:
+            if not linea.strip():
+                continue
+            datos = linea.strip().split(",")
+            paises.append({
+                "nombre": datos[0],
+                "superficie": int(datos[1]),
+                "poblacion": int(datos[2]),
+                "continente": datos[3]
+            })
+    return paises
+
+
+def agregar_paises():
     while True:
         try:
             numero_paises = int(
@@ -77,23 +91,60 @@ def agregar_paises(paises):
                 f"{nombre_pais},{superficie_pais},{poblacion_pais},{continente_pais}\n")
 
 
-def actualizar_paises(paises):
-    print("hola")
+def actualizar_paises():
+    paises = cargar_paises()
+
+    while True:
+        nombre = input(
+            "Nombre del pais que desea actualizar: ").strip().lower()
+
+        for pais in paises:
+            if pais["nombre"].lower() == nombre:
+                print(f"Pais encontrado: {pais['nombre']}")
+                print(f"Superficie: {pais['superficie']}")
+                print(f"Poblacion: {pais['poblacion']}")
+                print(f"Continente: {pais['continente']}")
+                break
+        else:
+            print("Pais no encontrado.")
+            continue
+
+        print("1. Nombre")
+        print("2. Superficie")
+        print("3. Poblacion")
+        print("4. Continente")
+
+        try:
+            opcion = int(input("Que caracteristica desea actualizar?: "))
+        except ValueError:
+            print("Opcion no válida.")
+            continue
+
+        nuevo_valor = input("Ingrese nuevo valor: ").strip()
+
+        if opcion == 1:
+            pais["nombre"] = nuevo_valor
+        elif opcion == 2:
+            pais["superficie"] = int(nuevo_valor)
+        elif opcion == 3:
+            pais["poblacion"] = int(nuevo_valor)
+        elif opcion == 4:
+            pais["continente"] = nuevo_valor
+        else:
+            print("Opcion no válida.")
+            continue
+
+        with open("paises.txt", "w") as archivo:
+            for p in paises:
+                archivo.write(
+                    f"{p['nombre']},{p['superficie']},{p['poblacion']},{p['continente']}\n")
+
+        print("Pais actualizado correctamente.")
+        break
 
 
 def buscar_pais():
-    paises = []
-    with open("paises.txt", "r") as archivo:
-        for linea in archivo:
-            if not linea.strip():
-                continue
-            datos = linea.strip().split(",")
-            paises.append({
-                "nombre": datos[0],
-                "superficie": float(datos[1]),
-                "poblacion": int(datos[2]),
-                "continente": datos[3]
-            })
+    paises = cargar_paises()
 
     nombre_pais = input("Ingresar nombre del pais: ")
     for pais in paises:
@@ -108,15 +159,86 @@ def buscar_pais():
 
 
 def filtrar_paises():
-    print("hola")
+    paises = cargar_paises()
+
+    print("1. Filtrar por Continente:")
+    print("2. Filtrar por Rango de poblacion:")
+    print("3. Filtrar por Rango de superficie:")
+    opcion = int(input("Ingrese opcion para filtrar:"))
+
+    if opcion == 1:
+        nombre = input("Ingrese nombre de continente")
+        for pais in paises:
+            if pais["continente"] == nombre:
+                print(pais)
+
+    if opcion != 1:
+        rango_minimo = int(input("Ingrese rango minimo"))
+        rango_maximo = int(input("Ingrese rango maximo"))
+
+        if opcion == 2:
+            for pais in paises:
+                if pais["poblacion"] > rango_minimo and pais["poblacion"] < rango_maximo:
+                    print(pais)
+
+        if opcion == 3:
+            for pais in paises:
+                if pais["superficie"] > rango_minimo and pais["superficie"] < rango_maximo:
+                    print(pais)
 
 
 def ordenar_paises():
-    print("hola")
+    paises = cargar_paises()
+    print("Ordenar países por:")
+    print("1. Nombre")
+    print("2. Población")
+    print("3. Superficie")
+    opcion = int(input("Ingrese opcion para ordenar: "))
+
+    if opcion == 1:
+        paises.sort(key=lambda p: p["nombre"])
+    elif opcion == 2:
+        paises.sort(key=lambda p: p["poblacion"])
+    elif opcion == 3:
+        ascendente = input("Orden ascendente o descentente ( A ó D ):")
+        if ascendente == "a":
+            paises.sort(key=lambda p: p["superficie"])
+        elif ascendente == "d":
+            paises.sort(key=lambda p: p["superficie"], reverse=True)
+
+    else:
+        print("Opcion no válida.")
+        return
+
+    for pais in paises:
+        print(f"{pais['nombre']} - Superficie: {pais['superficie']} - Poblacion: {pais['poblacion']} - Continente: {pais['continente']}")
 
 
 def mostrar_estadisticas():
-    print("hola")
+    paises = cargar_paises()
+    print("Mostrar estadisticas:")
+    print("1. Pais con mayor y menor poblacion:")
+    print("2. Promedio de poblacion:")
+    print("3. Cantidad de países por continente:")
 
+    opcion = int(input("Ingrese una opcion"))
+    if opcion == 1:
+        paises.sort(key=lambda p: p["poblacion"])
+    print(f"Pais con mayor poblacion {paises[0]}")
+    print(f"Pais con menoer poblacion {paises[-1]}")
+
+    if opcion == 3:
+        conteo = {}
+        for pais in paises:
+            continente = pais["continente"]
+
+            if continente in conteo:
+                conteo[continente] += 1
+            else:
+                conteo[continente] = 1
+
+
+for continente, cantidad in conteo.items():
+    print(f"{continente}: {cantidad}")
 
 menu()
